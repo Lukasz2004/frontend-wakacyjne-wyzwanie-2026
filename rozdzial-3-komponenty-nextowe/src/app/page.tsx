4bfb4a626319ee2d75d6dcf92d0d899bcf08cb9d
Lukasz2004 <lukaszczerwinski2004@gmail.com>
2026-08-25 18:55:46 +0200

    feat: add Next.js components to TeamMemberCard

--- rozdzial-3-komponenty-nextowe/src/app/page.tsx
+++ rozdzial-3-komponenty-nextowe/src/app/page.tsx
@@ -1,5 +1,7 @@
 import TeamMemberCard from "@/components/TeamMemberCard";
 
+import Script from "next/script";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-zinc-50 p-6">
@@ -9,6 +11,11 @@ export default function Home() {
         bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis arcu vitae erat sollicitudin, ac hendrerit neque auctor."
         skills={["React", "Next.js", "Tailwind CSS", "TypeScript"]}
       />
+        <Script id="hello">
+            {`
+          alert('Hejka!');
+        `}
+        </Script>
     </main>
   );
 }
